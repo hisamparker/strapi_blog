6f2781c47ff249aa0c6c31dd4f12b798e9179ef0
sam parker <hisamparker@gmail.com>
2022-12-14 18:44:49 +0100

    added styles and updated _app.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 //This allows us to access a remote image, but still use the built-in Next.js Image Optimization API
-export const reactStrictMode = true;
-export const images = {
-  loader: "default",
-  domains: ["localhost"],
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    loader: "default",
+    domains: ["localhost"],
+  },
 };
